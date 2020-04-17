37b6b95be7465875a9a66453602559ddb438f27f
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-17 17:54:41 +0200

    removed some comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,19 +7,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 class Dishdetail extends Component {
   render() {
     return (
-    //   <div className="row">
-    //   <div  className="col-12 col-md-5 m-1">
-    //     {this.renderDish(this.state.selectedDish)}
-    //   </div>
-    // </div>
-          <Card>
-            <CardImg top src={this.props.image} alt={this.props.name} />
-            <CardBody>
-                <CardTitle>{this.props.name}</CardTitle>
-                <CardText>{this.props.description}</CardText>
-            </CardBody>
-        </Card>
-      //  </div>
+        <Card>
+          <CardImg top src={this.props.image} alt={this.props.name} />
+          <CardBody>
+              <CardTitle>{this.props.name}</CardTitle>
+              <CardText>{this.props.description}</CardText>
+          </CardBody>
+      </Card>
     );
   }
 }
